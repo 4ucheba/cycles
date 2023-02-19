f7e226874e2565860cd26825bf6f2c6ecbb4847d
Test4 <4ucheba@mail.ru>
2023-02-19 17:31:52 +0300

    Initial commit

---
+++ src/main/java/org/netology/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //  CalcRestMonth service = new CalcRestMonth();
+        //  int RestMonth = service.calculate(100_000, 60_000, 150_000);
+        //  System.out.println(RestMonth);
+    }
+}
